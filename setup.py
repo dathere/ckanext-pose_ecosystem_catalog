1492fb8a7ff9afd75bd8430d1d79ea34a9c7d75b
tino097 <konstantin.sivakov@gmail.com>
2025-02-04 16:36:02 +0100

    Fixing the theme

--- setup.py
+++ setup.py
@@ -85,8 +85,8 @@ setup(
         pose_custom_homepage=ckanext.pose_theme.pose_custom_homepage.plugin:PoseThemeHomepagePlugin
         pose_custom_header=ckanext.pose_theme.pose_custom_header.plugin:PoseThemeHeaderPlugin
         pose_custom_footer=ckanext.pose_theme.pose_custom_footer.plugin:PoseThemeFooterPlugin
-        pose_custom_showcase=ckanext.pose_theme.pose_custom_showcase.plugin:
- 
+        pose_custom_showcase=ckanext.pose_theme.pose_custom_showcase.plugin:PoseShowcasePlugin
+
         pose_custom_heroslider=ckanext.pose_theme.pose_custom_heroslider.plugin:PoseHeroSliderPlugin
 
         pose_theme=ckanext.pose_theme.custom_themes.pose_theme.plugin:PoseThemePlugin
